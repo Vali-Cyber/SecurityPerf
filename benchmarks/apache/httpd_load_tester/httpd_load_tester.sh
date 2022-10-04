418f54309fe6eb6703e3517600ee219456eaf91b
Austin Gadient <austin.gadient@valicyber.com>
2022-10-03 20:30:41 -0600

    Tweaks and enhancements

--- benchmarks/apache/httpd_load_tester/httpd_load_tester.sh
+++ benchmarks/apache/httpd_load_tester/httpd_load_tester.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ab -n 1000000 -c 100 http://httpd/index.html &> /results/results.txt
+# 1,000,000 requests. It is easier to read this way
+NUM_REQUESTS=$((1000*1000))
+NUM_CONCURRENT_REQUESTS=100
+ab -n $NUM_REQUESTS -c $NUM_CONCURRENT_REQUESTS http://httpd/index.html &> /results/results.txt
